377b5db144dc441025cd2281cad4581322cbaca9
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2023-09-17 17:58:48 +0700

    fix types

--- packages/domain-event/src/DependencyInjection/RekalogikaDomainEventExtension.php
+++ packages/domain-event/src/DependencyInjection/RekalogikaDomainEventExtension.php
@@ -24,7 +24,7 @@ use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 class RekalogikaDomainEventExtension extends Extension
 {
     /**
-     * @param array<array-key,array<array-key,mixed>> $configs
+     * @param array<array-key,mixed> $configs
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
