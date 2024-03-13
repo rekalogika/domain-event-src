64a9f404de6f7f404171109ec0013f1cf6f19275
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-03-13 12:07:17 +0700

    chore: add missing typehints (#84)

--- packages/domain-event/src/RekalogikaDomainEventBundle.php
+++ packages/domain-event/src/RekalogikaDomainEventBundle.php
@@ -21,7 +21,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RekalogikaDomainEventBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
