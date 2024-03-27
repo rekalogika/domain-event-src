9fc3ac965dc0a86415876462c8c306cc3725b727
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-03-27 12:13:16 +0000

    fix: invalid version in `composer.json` (#85)

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,12 +3,7 @@
 declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
@@ -20,11 +15,6 @@ return static function (MBConfig $mbConfig): void {
     $mbConfig->workers([
         UpdateReplaceReleaseWorker::class,
         SetCurrentMutualDependenciesReleaseWorker::class,
-        // AddTagToChangelogReleaseWorker::class,
-        // TagVersionReleaseWorker::class,
-        // PushTagReleaseWorker::class,
-        // SetNextMutualDependenciesReleaseWorker::class,
         UpdateBranchAliasReleaseWorker::class,
-        // PushNextDevReleaseWorker::class,
     ]);
 };
