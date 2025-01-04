337213b2009f53033c5040894b4fe8ad1f38e084
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-01-04 18:07:55 +0700

    chore: static analysis fix

--- tests/Framework/Entity/User.php
+++ tests/Framework/Entity/User.php
@@ -18,6 +18,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class User implements UserInterface
 {
     /**
+     * @param non-empty-string $username
      * @param list<string> $roles
      */
     public function __construct(
